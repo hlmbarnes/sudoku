32c337e78508e84d469b0b360b71fadcb8dff1cf
Hannah Barnes <hlmbarnes@me.com>
2016-04-05 13:50:28 -0700

    created branch for git pages

--- style.js
+++ style.js
@@ -1,7 +1,10 @@
 $(document).ready(function(){
-
+	function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
     
 });
+
 
 var Array = [ 
 	[1,2,3,4,5,6,7,8,9],
@@ -19,8 +22,15 @@ var RandomArray =[];
 var hideBoxes = function(Array){
 	for (i=0; i<=8; i++) {
 		function getRandomNumHide(0, 5) {
-  	return Math.floor(Math.random() * 6) + 1;
- 		var hidden = function(
+  		return Math.floor(Math.random() * 6) + 1;
+ 			var hidden = function(math.floor(math.random()*6) +1){
+ 				for (j=0; j<(math.floor(math.random()*6+1)).length; j++){
+ 					$("#Puzzle_Gen").on('click', function(){
+ 						var randBox = getRandomInt(0,8);
+ 						var clearRow = 
+ 					}
+ 				}
+ 			}
 		
 		}
 
