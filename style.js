740404282a0b740f1ec35e6676202d4c53ceb30c
Hannah Barnes <hlmbarnes@me.com>
2016-04-04 22:43:50 -0700

    created a click event function for reference later to clear the box value, also created an array of arrays to represent all the different rows in the grid values 1-9

--- style.js
+++ style.js
@@ -1,7 +1,25 @@
 $(document).ready(function(){
-    $("[id^=Row]").on("click",function(e){
-        alert(e.target.id);
-    });
-    
 
+    
 });
+
+Var Array= [ 
+	[1,2,3,4,5,6,7,8,9],
+ 	[1,2,3,4,5,6,7,8,9],
+ 	[1,2,3,4,5,6,7,8,9], 
+  [1,2,3,4,5,6,7,8,9], 
+  [1,2,3,4,5,6,7,8,9], 
+  [1,2,3,4,5,6,7,8,9],
+  [1,2,3,4,5,6,7,8,9],
+  [1,2,3,4,5,6,7,8,9],
+  [1,2,3,4,5,6,7,8,9] ]
+
+// This function is a click to hide value
+    // $("[id^=Row]").on("click",function(e){
+    //     var clear= e.target.id;
+    //     alert("[id=" + clear + "]");
+    //     var tag= "[id=" + clear + "]" ;
+    //     // $(tag).text("");
+    //     tag.toggle();
+
+    // });
