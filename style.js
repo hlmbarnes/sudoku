aa9e47c35abf90e69dfb9c79e49ed71933a8d715
Hannah Barnes <hlmbarnes@me.com>
2016-04-05 10:23:56 -0700

    updated style.js to have the beining of the function to determine the hidden boxes in the Array of arrays representing the dfferent rows. I also created a button for Generate Puzzle that will serve to activate this hide functionality on click jquery

--- style.js
+++ style.js
@@ -14,6 +14,25 @@ Var Array= [
   [1,2,3,4,5,6,7,8,9],
   [1,2,3,4,5,6,7,8,9] ]
 
+var RandomArray =[];
+
+var hideBoxes (Array){
+	for (i=0; i<= 8; i++) {
+		function getRandomHide(0, 5) {
+  	return Math.floor(Math.random() * 6) + 1;
+ 		var hidden = function(
+		
+		}
+
+
+}
+
+
+// This is the random min and max function
+// function getRandomIntInclusive(min, max) {
+//   return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
+
 // This function is a click to hide value
     // $("[id^=Row]").on("click",function(e){
     //     var clear= e.target.id;
