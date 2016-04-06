baf63d83efb25f52ac730e9435461b918d332dbb
Hannah Barnes <hlmbarnes@me.com>
2016-04-06 10:01:52 -0700

    updated the EasyArray variable to have an array of arrays of solved solutions

--- style.js
+++ style.js
@@ -4,15 +4,15 @@ $(document).ready(function(){
 });
 
 var easyArray = [ 
-	[1,2,3,4,5,6,7,8,9],
- 	[1,2,3,4,5,6,7,8,9],
- 	[1,2,3,4,5,6,7,8,9], 
-  [1,2,3,4,5,6,7,8,9], 
-  [1,2,3,4,5,6,7,8,9], 
-  [1,2,3,4,5,6,7,8,9],
-  [1,2,3,4,5,6,7,8,9],
-  [1,2,3,4,5,6,7,8,9],
-  [1,2,3,4,5,6,7,8,9] ]
+	[8,2,7,1,5,4,3,9,6],
+ 	[9,6,5,3,2,7,1,4,8],
+ 	[3,4,1,6,8,9,7,5,2], 
+  [5,9,3,4,6,8,2,7,1], 
+  [4,7,2,5,1,3,6,8,9], 
+  [6,1,8,9,7,2,4,3,5],
+  [7,8,6,2,3,5,9,1,4],
+  [1,5,4,7,9,6,8,2,3],
+  [2,3,9,8,4,1,5,6,7] ]
 
 var RandomArray = easyArray.slice();
 
@@ -21,22 +21,17 @@ function getRandomInt(min, max) {
   }
 
 $("#Puzzle_Gen").on('click', function (){
-// var hideBoxes = function(easyArray){
   
   for (i=0; i<=8; i++) {
     var getRandomNumHide = getRandomInt(1, 5); 
     for (j=0; j<getRandomNumHide; j++){
       var hidden = getRandomInt(0,8);
       RandomArray[i][hidden]='';
-
       }
   }
-  // }
   alert(RandomArray);
 
-  // var box = $("#Row1Col1");
 
-  // $("#Row1Col1").text(10);
   for (i=0; i<=8; i++){
     for (j=0; j<=8; j++){
       var divA = $("#Row" + (i + 1) + "Col" + (j + 1));
